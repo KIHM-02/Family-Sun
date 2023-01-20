308ac6892c03a112d04287f2774092d6c2a994e9
Kevin Isaac Montero Huerta <81937772+KIHM-02@users.noreply.github.com>
2023-01-20 01:19:45 -0600

    Added display to the menu
    Added the content of display (us, products and contact)

--- calentadores/js/modal.js
+++ calentadores/js/modal.js
@@ -2,43 +2,34 @@ const linkNosotros = document.querySelector("#linkNosotros");
 const linkProductos = document.querySelector("#linkProductos");
 const linkContacto = document.querySelector("#linkContacto");
 
-const btnCerrar = document.querySelector("#btnCerrar");
+const btnCerrarNosotros = document.querySelector("#btnCerrarNosotros");
+const btnCerrarProd = document.querySelector("#btnCerrarProd");
+const btnCerrarContacto = document.querySelector("#btnCerrarContacto");
 
 const modalNosotros = document.querySelector("#nosotros-modal");
 const modalProductos  = document.querySelector("#productos-modal");
 const modalContacto = document.querySelector("#contactanos-modal");
 
-var tipoModal = 0;
-
 linkNosotros.addEventListener("click", ()=>{
     modalNosotros.showModal();
-    tipoModal = 1;
 });
 
 linkProductos.addEventListener("click", ()=>{
     modalProductos.showModal();
-    tipoModal = 2;
 });
 
 linkContacto.addEventListener("click", ()=>{
     modalContacto.showModal();
-    tipoModal = 3;
 });
 
-btnCerrar.addEventListener("click", ()=>{
-    switch(tipoModal)
-    {
-        case 1: 
-            modalNosotros.close();
-            tipoModal = 0;
-            break;
-        case 2:
-            modalProductos.close();
-            tipoModal = 0;
-            break;
-        case 3:
-            modalContactos.close();
-            tipoModal = 0;
-            break;
-    }
+btnCerrarNosotros.addEventListener("click", ()=>{
+  modalNosotros.close();
 });
+
+btnCerrarProd.addEventListener("click", ()=>{
+    modalProductos.close();
+})
+
+btnCerrarContacto.addEventListener("click", ()=>{
+    modalContacto.close();
+})
